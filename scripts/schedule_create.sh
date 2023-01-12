373c068d02fa7136ecbf6c4fa71e1235ed9a6b6d
Lee Doolan <lee.doolan@travisperkins.co.uk>
2023-01-12 09:42:29 +0000

    altered schedule command

--- scripts/schedule_create.sh
+++ scripts/schedule_create.sh
@@ -14,12 +14,14 @@ sleep 5
 bold "Create the schedules"
 gcloud --project="${PROJECT_ID}" scheduler jobs create http "${SCHEDULE_NAME}_1" --location="${REGION}"  --description="${SCHEDULE_DESCR}" \
 --schedule="${SCHEDULE_CRON}" --time-zone="Europe/London" \
---uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}?message=Dummy_Schedule_1" --http-method="post" \
---oidc-service-account-email="${IAM_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com" --oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}" \
---max-retry-attempts="0" --max-retry-duration="0s" --min-backoff="5s" --max-backoff="1h" --max-doublings="5"
+--http-method="post" \
+--uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}?message=Dummy_Schedule_1" \
+--oidc-service-account-email="${IAM_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com" \
+--oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}"
 
 gcloud --project="${PROJECT_ID}" scheduler jobs create http "${SCHEDULE_NAME}_2" --location="${REGION}"  --description="${SCHEDULE_DESCR}" \
 --schedule="${SCHEDULE_CRON}" --time-zone="Europe/London" \
---uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}?message=Dummy_Schedule_2" --http-method="post" \
---oidc-service-account-email="${IAM_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com" --oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}" \
---max-retry-attempts="0" --max-retry-duration="0s" --min-backoff="5s" --max-backoff="1h" --max-doublings="5"
+--http-method="post" \
+--uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}?message=Dummy_Schedule_2" \
+--oidc-service-account-email="${IAM_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com" \
+--oidc-token-audience="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION}"
